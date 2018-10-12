34e2c755d341581bd3d6444ea92e5fbe2cd9e66c
tamago324 <tamago_pad@yahoo.co.jp>
2018-10-12 14:00:59 +0900

    Add 2 practice

---
+++ 2/Practice05.java
@@ -0,0 +1,19 @@
+class Practice05 {
+    public static void main(String[] args) {
+        int number = 12;
+        
+        // numberが20より小さいとき、どちらでもないときの条件分岐を追加してください
+        if (number < 10) {
+            System.out.println("10より小さい");
+        }
+        
+    }
+}
+
+/* 手順
+ * 1. else if の追加(条件：numberが20より小さい)
+ *     1.1 「10以上、20より小さい」と出力
+ * 2. else の追加
+ *     2.1 「20以上」と出力
+ * 3. numberの値をいろいろ変えて、出力を確認する
+ * */
