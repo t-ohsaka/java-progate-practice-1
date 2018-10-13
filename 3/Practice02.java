7c9e1506b093c194601ee1eece20b40d51853812
tamago324 <tamago_pad@yahoo.co.jp>
2018-10-13 14:46:30 +0900

    Add file

---
+++ 3/Practice02.java
@@ -0,0 +1,16 @@
+class Practice02 {
+  public static void main(String[] args) {
+    // printDataメソッドを呼び出してください
+    
+    
+  }
+  
+  // printDataメソッドを定義してください
+  
+  
+}
+
+/* 手順
+ * 1.printDataメソッドで「私の名前はtaroです。」と出力させる
+ * 2.mainメソッド内でprintDataメソッドを呼び出す
+ */
---
+++ 3/Practice04.java
@@ -0,0 +1,19 @@
+class Practice04 {
+    public static void main(String[] args) {
+        // printDataに「自分の名前」と「自分の年齢」を渡して呼び出す
+
+    }
+
+    // 年齢に関する引数(age)を受け取れるようにしてください
+    static void printData(String name) {
+        System.out.println("私の名前は" + name + "です");
+        // 「年齢は◯◯歳です」と出力してください
+        System.out.println();
+        
+    }
+}
+
+/* 手順
+ * 1.printDataメソッドに整数(int)の年齢(age)という引数を受け取るように変更
+ * 2.名前と年齢を渡してprintDataを実行する
+ */
