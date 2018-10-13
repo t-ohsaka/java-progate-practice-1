f3d0f6969c798ff64ce47472af0883e2b9d64f7a
tamago324 <tamago_pad@yahoo.co.jp>
2018-10-13 11:28:01 +0900

    Add practice

---
+++ 2/Practice13.java
@@ -0,0 +1,17 @@
+class Practice13 {
+  public static void main(String[] args) {
+    String[] names = {"りんご", "ぶどう", "ばなな"};
+    
+    // for文を用いて、各要素を出力してください
+    
+    
+  }
+}
+
+/* 出力結果*
+
+りんご
+ぶどう
+ばなな
+
+*/
